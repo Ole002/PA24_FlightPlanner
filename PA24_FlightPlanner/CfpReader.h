58ddda9e0be4267a50f1aed69b7aaec68d331ace
Jan Ole Struthoff <janole.struthoff@gmail.com>
2018-01-21 11:02:09 +0100

    test commit from Visual Studio.

---
+++ PA24_FlightPlanner/CfpReader.h
@@ -0,0 +1,15 @@
+#pragma once
+class CfpReader {
+	/*
+	ToDo:
+		- finish
+	*/
+	/*
+	About CfpReader
+		temporary class; reads user's flight plan.
+		User's flight plan contains some route+weather info, from which
+		objects (CAircraft, CRoute, ...) are generated. These objects are
+		used for calculating the actual ofp.
+	*/
+
+};
